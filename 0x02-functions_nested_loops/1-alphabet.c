af1c787fe4781955f3c11081b5c5edaa8004c1b7
Bayan445 <bayan.khaled.04@gmail.com>
2023-08-30 14:22:14 -0700

    akhi

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,25 @@
+
+#include "main.h"
+/**
+ * main - Entry point
+ * Descreption: A program that prints the alphabet in lowercase
+ * Return: always 0 (Success)
+ */
+
+
+int main(void)
+{
+	void print_alphabet(void);
+	{
+		char EA;
+
+		for (EA = 'a'; EA <= 'z'; EA++)
+		{
+			putchar(EA)
+		}
+	}
+
+	return (0);
+
+
+}
