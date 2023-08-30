b7fdf7c5071b34a1f128993b99bc87d78310f911
Bayan445 <bayan.khaled.04@gmail.com>
2023-08-30 16:57:04 -0700

    lolr

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,2 +1,32 @@
 
+#include "main.h"
+/**
+ * main - Entry point
+ * Descreption: prints some useless characters
+ * Return: always 0 (Success)
+ */
 
+
+int main(void)
+{
+	void print_alphabet_x10(void);
+	{
+		int i;
+
+		for (i = 0 ; i < 10 ; i++)
+		{
+		int ea;
+
+		for (ea = 'a'; ea <= 'z'; ea++)
+		{
+			_putchar(ea);
+			_putchar('\n');
+		}
+		}
+	}
+
+	return (0);
+
+
+
+}
