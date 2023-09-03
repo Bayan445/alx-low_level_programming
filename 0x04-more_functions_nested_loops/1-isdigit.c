72fa394c9427a1a514ea6bafb6b6130a7fe6d35d
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-03 12:24:53 -0700

    noop

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -9,7 +9,7 @@
 
 int _isdigit(int c)
 {
-	if ( _isdigit(c) && 0<c<9 )
+	if ( _isdigit(c) && 0 <= c && c <= 9 )
 	{
 		return (1);
 	}
@@ -17,4 +17,5 @@ int _isdigit(int c)
 	{
 		return (0);
 	}
+
 }
