909830f0829428012098021ca58dca6401ae8491
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-04 01:05:42 -0700

    luca

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * mul - the main function
+ * @a: number to check
+ * @b: number to multi
+ * Descreption: checks if the character is capital or not
+ * Return:  0 (Success)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+
+}
