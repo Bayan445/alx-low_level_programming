82ccf855f9f96a31f26049b49157abb082773b26
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-04 10:02:02 -0700

    klop

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - the first function
+ * print_numbers - check the code
+ *
+ * Return: Always 0.
+ */
+
+void print_numbers(void)
+{
+		char i;
+
+		for (i = 0; i < 10; i++)
+		{
+			_putchar(i + 48);
+			_putchar('\n');
+		}
+}
+int main(void)
+{
+	return (0);
+}
--- 0x04-more_functions_nested_loops/_putchar.c
+++ 0x04-more_functions_nested_loops/_putchar.c
@@ -9,7 +9,7 @@
  * Return: on succes 1.
  * on error, -1 is returned.
  */
-int _putchar(char c)
+int _putchar(int c)
 {
 	return (write(1, &c, 1));
 }
