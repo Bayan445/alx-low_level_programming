ae8c2230d62b293189e400e6602936638fb3ea05
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-06 15:05:50 -0700

    pubg

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * _strlen - a function tha tell the size
+ * @s:a pointer name.
+ * Return: len
+ */
+
+int _strlen(char *s)
+{
+	int len = 0;
+
+	len = strlen(s);
+	return (len);
+}
+
