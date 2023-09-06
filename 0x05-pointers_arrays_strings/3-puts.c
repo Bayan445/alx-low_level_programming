212b9c3520e060cbabd108fdcd11b7149e2f6908
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-06 15:23:51 -0700

    lack of luck{

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <string.h>
+
+
+/**
+ * _puts - a function tha tell the size
+ * @str:a pointer name.
+ * Return: (0) Always
+ */
+
+void _puts(char *str)
+{
+	puts(str);
+
+}
