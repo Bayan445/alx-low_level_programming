5808cbeb9458245c3eb12e14d67cbee58ab46218
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-09 22:11:25 -0700

    by hand

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * puts2 - Reverses a string
+ * @str: Input string
+ */
+
+void puts2(char *str)
+{
+	int i;
+	int length;
+
+
+	length = strlen(str);
+
+	for (i = 0; i <= length - 1; i++)
+	{
+		if (i %2 == 0 )
+		{
+			_putchar(str[i]);
+		}
+		_putchar('\n');
+	}
+}
