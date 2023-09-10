64c2074631ef6c966ee26ea73500abebfc1832b4
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-09 22:18:01 -0700

    sala

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -16,10 +16,11 @@ void puts2(char *str)
 
 	for (i = 0; i <= length - 1; i++)
 	{
-		if (i %2 == 0 )
+		if (i % 2 == 0)
 		{
 			_putchar(str[i]);
 		}
-		_putchar('\n');
 	}
+	_putchar('\n');
 }
+
