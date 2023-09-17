cc94972318d910068063b97908642a7764a4528b
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-17 13:36:03 -0700

    powerplus

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - a power function give power of number
+ *
+ * @x: adigit to expression
+ * @y: a digit wich is the power 
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+
+	else if (y == 0)
+	{
+		return (1);
+	}
+
+	else if (y == 1)
+	{
+		return (x);
+	}
+
+	return (x * _pow_recursion(x, y - 1));
+}
+
