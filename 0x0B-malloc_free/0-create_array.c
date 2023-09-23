07948db975196190280c1035709e2770c4e5b067
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-23 13:34:00 -0700

    tiyred

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -1,5 +1,6 @@
 #include "main.h"
 #include <stdlib.h>
+
 /**
  * create_array - create array of size size and assign char c
  * @size: size of array
@@ -8,6 +9,8 @@
  * Return: pointer to array, NULL if fail
  *
  */
+
+
 char *create_array(unsigned int size, char c)
 {
 	char *str;
@@ -21,4 +24,3 @@ char *create_array(unsigned int size, char c)
 		str[i] = c;
 	return (str);
 }
-
--- 0x0B-malloc_free/_putchar.c
+++ 0x0B-malloc_free/_putchar.c
@@ -1,3 +1,4 @@
+
 #include "main.h"
 #include <unistd.h>
 /**
@@ -9,6 +10,5 @@
  */
 int _putchar(char c)
 {
-	return (write(1, &c, 1));
+        return (write(1, &c, 1));
 }
-
