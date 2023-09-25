8b3a75a842c82dbb53d6e67d6bdfe9dbf4ba34f5
Bayan445 <bayan.khaled.04@gmail.com>
2023-09-25 13:34:19 -0700

    allahu akbar

---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include <string.h>
+#include <stdlib.h>
+
+
+/**
+ * str_concat - A function that will mix tow strings
+ *
+ * @s1: the first string
+ * @s2: the second string that will be added
+ *
+ * Description: it is just a task will make tow one
+ *
+ * Return: A pointer to a newly allocated string
+ */
+
+char *str_concat(char *s1, char *s2)
+{
+	char *taymaa = malloc(strlen(s1) + strlen(s2) + 1);
+
+	strcpy(taymaa, s1);
+	strcat(taymaa, s2);
+
+	if (s1 == NULL || s2 == NULL || taymaa == NULL)
+	{
+		return (NULL);
+	}
+	return (taymaa);
+}
