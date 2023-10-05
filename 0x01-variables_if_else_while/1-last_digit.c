cfb0f12fbdba9abbc79801eb0ef0d7cf7e4feac7
bayan445 <bayan.khaled.04@gmail.com>
2023-10-05 07:50:18 -0700

    tomatoes

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -3,7 +3,7 @@
 #include <stdio.h>
 
 /**
- * main - the entry point of the program 
+ * main - the entry point of the program
  *
  * Return: always 0.
  */
@@ -11,23 +11,23 @@
 int main(void)
 {
 	int n;
-	int taymaa;
+	int t;
 
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
-	taymaa = n % 10;
+	t = n % 10;
 
-	if (taymaa > 5)
+	if (t > 5)
 	{
-		printf("the last digit of %d is %d and is greater than 5 \n", n, taymaa);
+		printf("the last digit of %d is %d and is greater than 5 \n", n, t);
 	}
-	if (taymaa == 0)
+	if (t == 0)
 	{
-		printf("the last digit of %d is %d and is 0\n", n, taymaa);
+		printf("the last digit of %d is %d and is 0\n", n, t);
 	}
-	if (taymaa < 6 && taymaa != 0)
+	if (t < 6 && t != 0)
 	{
-		printf("the last digit of %d is %d and is less than 6 and not 0 \n", n, taymaa);
+		printf("the last digit of %d is %d and is less than 6 and not 0 \n", n, t);
 	}
 
 	return (0);
