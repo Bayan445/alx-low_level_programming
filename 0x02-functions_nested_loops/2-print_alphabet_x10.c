ba037256fd1e33a36ba6e1087c7b962aeb8d0ea7
bayan445 <bayan.khaled.04@gmail.com>
2023-10-06 07:29:36 -0700

    i hate pizza

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,27 +1,18 @@
-
-
 #include "main.h"
 
-/**
- * print_alphabet_x10 - prints 10 times the alphabet, in lowercase,
- * followed by a new line
- */
 void print_alphabet_x10(void)
 {
-	char ch;
-	int i;
-
-	i = 0;
+	int i = 0;
+	char pe;
 
 	while (i < 10)
 	{
-		ch = 'a';
-		while (ch <= 'z')
+		for (pe = 'a'; pe <= 'z'; pe++)
 		{
-			_putchar(ch);
-			ch++;
+			_putchar (pe);
 		}
-		_putchar('\n');
+		_putchar ('\n');
+
 		i++;
 	}
 }
