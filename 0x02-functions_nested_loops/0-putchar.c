24f00be3cff2273c034938c4f041cc48621cd8af
bayan445 <bayan.khaled.04@gmail.com>
2023-10-06 02:38:03 -0700

    universe

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,25 +1,24 @@
-
-
 #include "main.h"
-/**
- * main - Entry point
- * Descreption: prints some useless characters
- * Return: always 0 (Success)
- */
 
+/**
+ *
+ *
+ *
+ *
+ *
+ */
 
 int main(void)
 {
-	_putchar('_');
-	_putchar('p');
-	_putchar('u');
-	_putchar('t');
-	_putchar('c');
-	_putchar('h');
-	_putchar('a');
-	_putchar('r');
+
+	int i;
+	char arr[] = "_putchar";
+
+	for (i = 0; i < 9; i++)
+	{
+		_putchar(arr[i]);
+	}
+
 	_putchar('\n');
-
-	return (0);
-
+	return(0);
 }
