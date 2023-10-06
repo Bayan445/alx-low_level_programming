2e3b82b695b1ad02b83ec4d67a4b33d5415e0996
bayan445 <bayan.khaled.04@gmail.com>
2023-10-06 13:35:08 -0700

    isol

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ */
+int _abs(int n)
+{
+	int K = -n;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	else if (n > 0)
+	{
+		return (n);
+	}
+	else
+
+		return(K);
+}
