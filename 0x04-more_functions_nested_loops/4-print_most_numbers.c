c5fc11104c7be1b808c902ad0273be37c43941f2
bayan445 <bayan.khaled.04@gmail.com>
2023-10-07 13:13:14 -0700

    night7

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -8,13 +8,13 @@
 
 void print_most_numbers(void)
 {
-	char i;
+	int i;
 
 	for (i = 0; i < 10; i++)
 	{
 		if (i != 2 && i != 4)
 		{
-			_putchar(i + 48);
+			_putchar(i + '0');
 		}
 	}
 	_putchar('\n');
