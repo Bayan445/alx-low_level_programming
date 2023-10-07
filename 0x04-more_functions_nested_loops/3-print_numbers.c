f96dfbfc6bc9b862b99a26ccd082575ded3fd752
bayan445 <bayan.khaled.04@gmail.com>
2023-10-07 12:47:03 -0700

    night 5

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -1,19 +1,20 @@
 #include "main.h"
 
 /**
- * print_numbers - check the code
- * Descreption: a function that prints the numbers
- * Return: Always 0.
+ *
+ *
+ *
+ *
+ *
  */
 
 void print_numbers(void)
 {
-		char i;
-
-		for (i = 0; i < 10; i++)
-		{
-			_putchar(i + 48);
-		}
-		_putchar('\n');
+	int i;
+	
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
 }
-
