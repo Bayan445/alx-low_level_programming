458c96d9d6f2c5f58fbd614cc2b41ab61ca1837f
bayan445 <bayan.khaled.04@gmail.com>
2023-10-14 02:58:45 -0700

    stomach

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -1,15 +1,17 @@
 #include "main.h"
 
 /**
- * swap_int - a function that swaps 2 values
- * @a:a pointer name.
- * @b:a pointr too.
- * Return: void
+ * swap_int - function that swaps two values
+ *
+ * @a: the first value to be swapped
+ *
+ * @b: the secconed vale to be swapped
  */
 
 void swap_int(int *a, int *b)
 {
-	int temp = *a;
+
+	int Box = *a;
 	*a = *b;
-	*b = temp;
+	*b  = Box;
 }
