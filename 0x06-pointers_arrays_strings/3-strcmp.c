f939417da02926a28af10af0c4453e691d540245
bayan445 <bayan.khaled.04@gmail.com>
2023-10-17 09:45:38 -0700

    jarr

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,16 +8,30 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		return (15);
+		if (*s1 > *s2)
+		{
+			return (15);
+		}
+		
+		else if (*s2 > *s1)
+		{
+			return (-15);
+		}
+		s1++;
+		s2++;
+
 	}
 
-	else if (s2 > s1)
+	while (*s1 != '\0' && *s2 != '\0')
 	{
-		return (-15);
-	}
+		if (*s1 == *s2)
+			return (0);
 
-	else
-		return (0);
+		s1++;
+		s2++;
+
+	}
+	return (0);
 }
