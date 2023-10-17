b89a9ef8f5026b1b18c3809aecce0dcce222cb1a
bayan445 <bayan.khaled.04@gmail.com>
2023-10-17 09:59:15 -0700

    klouy

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -24,14 +24,8 @@ int _strcmp(char *s1, char *s2)
 
 	}
 
-	while (*s1 != '\0' && *s2 != '\0')
-	{
-		if (*s1 == *s2)
+	if (*s1 != '\0' && *s2 != '\0')
 			return (0);
 
-		s1++;
-		s2++;
-
-	}
-	return (0);
+	return (-15);
 }
