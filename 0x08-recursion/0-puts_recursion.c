eaf309c4bcc28cccf19d4c53a7eb4a55d18bae11
bayan445 <bayan.khaled.04@gmail.com>
2023-10-24 23:03:01 -0700

    aya

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - a function that prints a string
+ * using recursion way.
+ *
+ * @s: a string to be printed
+ *
+ */
+
+void _puts_recursion(char *s)
+{
+
+        if (*s == '\0')
+        {
+                _putchar('\n');
+
+        }
+
+        else
+        {
+                _putchar(*s);
+                _puts_recursion(s + 1);
+
+        }
+
+}
+
