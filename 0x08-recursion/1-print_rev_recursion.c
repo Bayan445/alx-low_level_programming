3f9142ae95e8ad897f5c98f3de83f1004efb19fe
bayan445 <bayan.khaled.04@gmail.com>
2023-10-25 04:33:41 -0700

    goh

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,9 +2,9 @@
 #include <string.h>
 
 /**
+ * _print_rev_recursion - a tubid recursive function
  *
- *
- *
+ * @s: a string to be printed in reverse
  *
  */
 
@@ -13,8 +13,12 @@ void _print_rev_recursion(char *s)
 
 	if (*s != '\0')
 	{
-		_print_rev_recursion(s + 1); /*it moves to the next character
-		so when it = to null it stop*/
+		_print_rev_recursion(s + 1);
+		/**
+		 * it moves to the next character
+		 * so when it = to null it stop
+		 */
+
 		_putchar(*s);
 
 	}
