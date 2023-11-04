1d870c4ec673b5b5b7d69da3ee08d9a585d97df4
bayan445 <bayan.khaled.04@gmail.com>
2023-11-04 00:35:15 -0700

    one hour

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -23,12 +23,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	if (s2 == NULL)
 		s2 = "";
 
-	r = strlen (s2);
-	b = strlen (s1);
-	pois = malloc (sizeof(char) * (b + n + 1));
+	r = strlen(s2);
+	b = strlen(s1);
+	pois = malloc(sizeof(char) * (b + n + 1));
 
 	if (pois == NULL)
-		return(NULL);
+		return (NULL);
 
 	/*copying s1 elements to new allocated pointer*/
 	for (e = 0; e < b; e++)
@@ -43,7 +43,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 		pois[e] = s2[r];
 		e++;
 	}
-	
+
 	pois[e] = '\0';
 
 	return (pois);
